f308d18eab6ba510809e154ead354c55a985418f
KaueSena01 <86299739+KaueSena01@users.noreply.github.com>
2021-08-09 16:47:14 -0300

    second commit

--- src/components/PasswordRevealing.vue
+++ src/components/PasswordRevealing.vue
@@ -5,7 +5,7 @@
           <ion-icon name="log-out-outline"></ion-icon>
         </div>
         <h1>Login</h1>
-        <p>Faça seu login</p>
+        <hr/>
         <div class="form-items">
           <label for="Nome">Nome:</label>
           <input type="text" placeholder="Seu nome completo" class="input-area">
@@ -15,17 +15,18 @@
           <label for="Senha">Senha:</label>
           <input 
           :type="inputType" 
-          placeholder="Digite uma senha segura" class="input-area" >
+          placeholder="Digite uma senha" class="input-area" >
 
           <div class="Revealing" @click="togglePassword">
 
           <ion-icon v-if="isPassword" name="eye-outline"></ion-icon>
           <ion-icon v-else name="eye-off-outline"></ion-icon>
           </div>
-        </div>
 
+        </div>
+        <a href="#">Esqueceu a senha?</a>
         <div class="form-items">
-          <input type="submit" class="input-area" value="Enviar">
+          <button type="submit" class="input-area">Enviar</button>
         </div>
       </form>
     </div>
@@ -81,7 +82,6 @@
   form{
     width: 95%;
     margin: 0 auto;
-    text-align: center;
   }
   form .logo{
     max-width: 30px;
@@ -91,35 +91,73 @@
   .logo ion-icon{
     width: 30px;
     height: 30px;
-    color: #64acba;
+    color: #3fc5df;
   }
   form h1{
     font-size: 30px;
-    color: #64acba;
+    color: #3fc5df;
+    text-align: center;
   }
-  form p{
-    font-size: 14px;
-    margin-bottom: 20px;
+  form hr{
+    margin: 0 35% 20px 35%;
+    width: 30%;
+    height: 2px;
+    background-color: #c3c3c3;
+    margin-bottom: 25px;
   }
   .form-items{
     width: 100%;
     height: 60px;
-    margin-top: 5px;
+    margin-top: 10px;
     display: flex;
     flex-direction: column;
   }
   .form-items label{
     font-size: 13px;
+    font-weight: 600;
     text-align: left;
+    color: #7d7e7e;
   }
   .form-items input{
     width: 100%;
     height: 38px;
-    background-color: #f9f9f9;
+    border: 2px solid #eee;
+    border-radius: 3px;
     padding-left: 8px;
     font-size: 13px;
+    transition: all .2s ease;
   }
-  .form-items ion-icon{
+  .form-items input:focus{
+    border: 2px solid #3fc5df;
+    padding-left: 15px;
+  }
+  .Revealing{
     position: absolute;
+    margin: 30px 300px;
+  }
+  .Revealing ion-icon{
+    color: #c3c3c3;
+  }
+  form a{
+    font-size: 13px;
+    text-decoration: none;
+    color: #15a1bc;
+  }
+  button{
+    margin-top: 7px;
+    width: 30%;
+    padding: 7px;
+    background-color: #3fc5df;
+    border: 2px solid #3fc5df;
+    border-radius: 3px;
+    font-weight: bold;
+    color: #fff;
+    cursor: pointer;
+    transition: all .2s ease;
+  }
+  button:hover{
+    opacity: 0.9;
+    border: 2px solid #15a1bc;
+    border-radius: 3px;
   }
 </style>
